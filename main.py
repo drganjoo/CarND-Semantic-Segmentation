ef8be62b9c45f6e76272a2899e587cdb58f82f3a
Fahad Zubair <fahadzubair@gmail.com>
2017-12-26 09:14:31 +0500

    changes

--- main.py
+++ main.py
@@ -26,14 +26,29 @@ def load_vgg(sess, vgg_path):
     """
     # TODO: Implement function
     #   Use tf.saved_model.loader.load to load the model and weights
+
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+
+    graph = tf.get_default_graph()
+
+    for op in graph.get_operations():
+        print(op)
+    print(tf.trainable_variables())
+
+    w1 = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3 = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4 = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7 = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+    return w1, keep_prob, layer3, layer4, layer7
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,7 +62,24 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+    regularizer = tf.contrib.layers.l2_regularizer(1e-3)
+
+    # conv_1_1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding = 'same',
+    #                             kernel_regularizer=regularizer)
+    conv_1_1 = vgg_layer7_out
+    output = tf.layers.conv2d_transpose(conv_1_1, num_classes, 4, 2, padding = 'same',
+                                kernel_regularizer=regularizer)
+
+    l4 = vgg_layer4_out
+    output = tf.add(output, l4)
+    output = tf.layers.conv2d_transpose(output, num_classes, 4, 2, padding = 'same',
+                                kernel_regularizer=regularizer)
+
+    l3 = vgg_layer3_out
+    output = tf.add(output, l3)
+    output = tf.layers.conv2d_transpose(output, num_classes, 16, 8, padding = 'same',
+                                kernel_regularizer=regularizer)
+    return output
 tests.test_layers(layers)
 
 
